84c72588c49ffbe9732df89d2cfd82b9a9248a5c
boubacardraga123-sketch <boubacardraga123@gmail.com>
2026-07-23 16:05:31 +0000

    Create sw-prof.js

---
+++ sw-prof.js
@@ -0,0 +1,36 @@
+// Service Worker - Espace Professeur
+const CACHE_NAME = 'espace-prof-v1';
+const CORE_ASSETS = [
+  './',
+  './espace-prof.html'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS).catch(() => {}))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Stratégie : réseau d'abord, repli sur le cache si hors-ligne
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./espace-prof.html')))
+  );
+});
